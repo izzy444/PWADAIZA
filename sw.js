fc556218089cf6d4b7f7b630966e5c36094e928b
izzy444 <izabelle.soares@etec.sp.gov.br>
2020-11-06 14:06:53 -0300

    apaga tuuudo

--- sw.js
+++ sw.js
@@ -1,34 +1,20 @@
 var CACHE_NAME = 'izabelle-v1'
-var urlsToCache = [
-'./css/izabellebraga.css',
-'./css/bootstrap.css',
-'./img/favicon.png ',
-'./js/bootstrap.js',
-'./js/jquery-3.5.1.min.js',
-'./libs/swweetalert2-master/src/sweetalert2.js',
-] 
-
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-       caches.open(CACHE_NAME)
-       .then(function(cache){
-        console.log('Cache aberto...')
-        return cache.addAll(urlsToCache)
-       })
-    )
-})
-
-self.addEventListener('fetch', function(event){
-    event.respondewith(
-        caches.match(event.request)
-        .then(function(response){
-            if(response){
-            return response
-            }
-            return fetch(event.request)
-        }
-    
-    )
-    )
-
-})
+var filesToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
